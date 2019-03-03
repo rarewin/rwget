77ce85ef5440da3a9d87f39bba768e1bfb394f22
Katsuki Kobayashi <rare@tirasweel.org>
2019-03-03 19:16:14 +0900

    get status code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+extern crate clap;
+extern crate hyper;
+
+use hyper::rt::{self, Future}; // Stream
+use hyper::{Client, Uri};
+// use std::io::{self, Write};
+
+use clap::{App, Arg};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("rwget")
+        .arg(Arg::with_name("URI").required(true))
+        .get_matches();
+
+    let uri: Uri = matches
+        .value_of("URI")
+        .expect("no URI")
+        .parse()
+        .expect("invalid URI");
+
+    rt::run(rt::lazy(|| {
+        let client = Client::new();
+        client
+            .get(uri)
+            .map(|res| {
+                println!("Response: {}", res.status());
+            })
+            .map_err(|err| {
+                println!("Error: {}", err);
+            })
+    }));
 }
